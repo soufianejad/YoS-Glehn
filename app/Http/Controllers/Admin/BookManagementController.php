935768760df558c8fc17c152e7cf943651a9d3ae
Soufiane Jadroun <soufiane.jad@gmail.com>
2026-03-22 11:59:26 +0000

    Class "App\Http\Controllers\Admin\Book" not found

--- app/Http/Controllers/Admin/BookManagementController.php
+++ app/Http/Controllers/Admin/BookManagementController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Setting;
 use App\Models\User;
+use App\Models\Book;
 use App\Services\NotificationService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
