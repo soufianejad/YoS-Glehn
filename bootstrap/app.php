547a10886d7167c5d35847303918508a9b8dc8f9
Soufiane Jadroun <soufiane.jad@gmail.com>
2026-03-28 22:14:49 +0000

    Add CheckAdultAccessExpiration middleware to web middleware stack

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
-
+            \App\Http\Middleware\CheckAdultAccessExpiration::class,
         ]);
 
         $middleware->alias([
