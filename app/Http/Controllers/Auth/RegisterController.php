f897dedd15775cd276fe0c6d53568088ed77b8d0
Soufiane Jadroun <soufiane.jad@gmail.com>
2026-04-20 19:04:59 +0100

    ✨ Add button component to WhatsApp OTP message template

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -114,28 +114,40 @@ class RegisterController extends Controller
                 $sent = false;
                 foreach ($targetNumbers as $number) {
                     try {
-                        $data = [
-                            'messaging_product' => 'whatsapp',
-                            'to' => $number,
-                            'type' => 'template',
-                            'template' => [
-                                'name' => 'otp_verification_code',
-                                'language' => [
-                                    'code' => 'en'
-                                ],
-                                'components' => [
-                                    [
-                                        'type' => 'body',
-                                        'parameters' => [
-                                            [
-                                                'type' => 'text',
-                                                'text' => (string) $code
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ];
+                       $data = [
+    'messaging_product' => 'whatsapp',
+    'to' => $number,
+    'type' => 'template',
+    'template' => [
+        'name' => 'otp_verification_code',
+        'language' => [
+            'code' => 'en'
+        ],
+        'components' => [
+            [
+                'type' => 'body',
+                'parameters' => [
+                    [
+                        'type' => 'text',
+                        'text' => (string) $code
+                    ]
+                ]
+            ],
+            // ✅ Add this button component
+            [
+                'type' => 'button',
+                'sub_type' => 'url',
+                'index' => '0',
+                'parameters' => [
+                    [
+                        'type' => 'text',
+                        'text' => (string) $code
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
                         $response = $client->request('POST', 'https://graph.facebook.com/v21.0/' . $phoneNumberId . '/messages', [
                             'body' => json_encode($data),
                             'headers' => [
